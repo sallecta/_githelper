ab0919650c761bed6920dec6c44d50e06021c779
sallecta <sallecta@inbox.ru>
2019-11-09 21:56:18 +0300

    can be called from child dir as ../gitpush.sh up $PWD

--- gitpush.sh
+++ gitpush.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-#version 1.6
-git add *
-git add **/.gitignore
-git add .gitignore
-git add -u
-echo 'Check status:' 
-git status
+#version 1.7
 
-if [ "$1" = "withCommit" ]
-then
-    commmitMessageArg="update gitpush script"
-    echo "- press Enter to accept commit message \"$commmitMessageArg\" and upload (push);"
-    echo '- type new non empty commit message and press Enter to upload (push);'
+fn_stoponerror () {
+if [ $1 -ne 0 ]; then
+        lNo=$(expr $2 - 1)
+        printf "  [Error at line No $lNo. $2]\n\n"
+        exit
 else
-    echo "- press Enter to upload (push) with commit message \"...\""
-    echo '- type commit message and press Enter to upload (push);'
+   printf "    [Success]\n\n"
+fi
+}
+
+if [ "$1" = "up" ]
+then
+    echo " - called from child folder. Checking child path"
+	if [ ! -d "$2" ]; then
+	  echo "   -- child path does not exist. Exit"
+	  exit 1
+	else
+	  mypath="$PWD"
+	  childpath="$2"
+	  echo "   -- child path exist, changing woring directory to $childpath"
+	  cd $childpath
+	  fn_stoponerror "$?" $LINENO
+	fi    
 fi
 
+git add *
+fn_stoponerror "$?" $LINENO
+git add .gitignore
+fn_stoponerror "$?" $LINENO
+git add .gitignore
+fn_stoponerror "$?" $LINENO
+git add -u
+fn_stoponerror "$?" $LINENO
+echo 'Check status:' 
+git status
+fn_stoponerror "$?" $LINENO
+
+echo "- press Enter to upload (push) with commit message \"...\""
+echo '- type commit message and press Enter to upload (push);'
 echo '- type Ctrl+C to cancel.'
 read commmitMessageUsr
 
@@ -37,6 +60,7 @@ fi
 echo commit message is \"$commmitMessage\"
 
 git commit --message="$commmitMessage"
+fn_stoponerror "$?" $LINENO
 
 git push
 
